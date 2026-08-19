3e661e8c1852026a08a15980ea2464cd45c8ce94
lautaroaguilar18 <lautaroaguilar18@outlook.com>
2026-08-19 01:32:57 -0300

    Practica 0: Ejercicios

---
+++ src/practica0/Funciones.java
@@ -0,0 +1,45 @@
+package practica0;
+
+public class Funciones {
+	
+	// Función que resuelve el ejercício 1
+	public static void saludarMundo() {
+		System.out.println("¡Hola, mundo!");
+	}
+	
+	// Función para el ejercicio 2
+	public static void saludarUsuario(String nombre) {
+		System.out.println("Hola " + nombre);
+	}
+		
+	// Función para el ejercicio 3
+	public static void sumar(int n1, int n2) {
+		int resultado = n1 + n2;
+		System.out.println("El resultado es " + resultado);
+	}
+	
+	// Función para el ejercicio 5
+	public static void promedio(double n1, double n2) {
+		double resultado = (n1 + n2)/2;
+		System.out.println("El promedio es " + resultado);
+	}
+	
+	// Función para el ejercicio 8
+	
+	public static void ponerNota(double x, double y) {
+		double resultado = (x + y)/2;
+		
+		if(resultado >= 7) {
+			System.out.println("Promocionado");
+		}
+		else if(resultado >= 4) {
+			System.out.println("Aprobado");
+		}
+		else {
+			System.out.println("Debe Recuperar");
+		}
+	
+	}
+
+}
+
---
+++ src/practica0/principal.java
@@ -0,0 +1,59 @@
+package practica0;
+
+import java.util.Scanner;
+
+public class principal {
+	public static void main(String[] args) {
+		// Ejercicio 2
+		
+		Scanner scan = new Scanner(System.in);
+		/*System.out.println("Ingrese su nombre: ");
+		String nombreDeusuario = scan.nextLine();
+		Funciones.saludarUsuario(nombreDeusuario);
+		
+		
+		// Ejercicio 3 
+		
+		/*System.out.println("Ingrese n1");
+		int n1 = scan.nextInt();
+		
+		System.out.println("Ingrese n2");
+		int n2 = scan.nextInt();
+		
+		Funciones.sumar(n1, n2);*/
+		
+		
+		
+		// Ejercicio 5
+		
+		/*System.out.println("Ingrese n1");
+		double n1 = scan.nextInt();
+		
+		System.out.println("Ingrese n2");
+		double n2 = scan.nextInt();
+		
+		Funciones.promedio(n1, n2);*/
+		
+		
+		
+		// Ejercicio 8
+		
+		System.out.println("Ingrese x");
+		double x = scan.nextDouble();
+		
+		System.out.println("Ingrese y");
+		double y = scan.nextDouble();
+		
+		Funciones.ponerNota(x, y);
+		
+		scan.close();
+		
+		
+		
+	
+	
+	
+	
+	
+	}
+}
